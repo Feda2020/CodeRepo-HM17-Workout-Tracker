64c9d1f93b540768112d6aaf9a0cd19fc8dc1559
Feda Zidan <fido311@gmail.com>
2020-11-15 20:07:28 -0600

    added html route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,55 +1,54 @@
-const router = require("express").Router();
-const db = require("../models/")
+const router = require('express').Router();
+const db = require('../models/');
 
-//Get the previous workouts
-router.get("/api/wourkouts", (req, res) => {
-    db.Workout.find({})
+// Get previous workouts
+router.get('/api/workouts', (req, res) => {
+  db.Workout.find({})
     .then(dbWorkout => {
-        console.log(dbWorkout);
-        res.json(dbWorkout);
+      console.log(dbWorkout);
+      res.json(dbWorkout);
     })
     .catch(err => {
-        res.status(400).json(err);
+      res.status(400).json(err);
     });
 });
 
-//Add the new workouts
-routers.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body)
+// Adding the new workout
+router.post('/api/workouts', ({ body }, res) => {
+  db.Workout.create(body)
     .then(dbWorkout => {
-        res.json(dbWorkout);
+      res.json(dbWorkout);
     })
     .catch(err => {
-        res.status(400).json(err);
+      res.status(400).json(err);
     });
 });
 
-//Update previous workouts
-router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.updateOne(
-        {_id: req.params.id },
-        {$push: { exercises: req.body } }
-    )
+//update workouts by id
+router.put('/api/workouts/:id', (req, res) => {
+  db.Workout.updateOne(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  )
     .then(dbWorkout => {
-        res.json(dbWorkout);
+      res.json(dbWorkout);
     })
     .catch(err => {
-        res.json(err);
+      res.json(err);
     });
 });
-
-//Get the most recent 5 workouts 
-router.get("/api/wourkouts/range", (req, res) => {
-    db.Workout.find({})
+// Get most recent 5 workouts
+router.get('/api/workouts/range', (req, res) => {
+  db.Workout.find({})
     .sort({ _id: -1 })
     .limit(5)
     .then(dbWorkout => {
-        console.log(dbWorkout);
-        res.json(dbWorkout);
-        console.log(dbWorkout);
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+      console.log(dbWorkout);
     })
     .catch(err => {
-        res.json(err);
+      res.json(err);
     });
 });
 
